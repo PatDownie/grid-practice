3bb716f6fe697e9816bcff87e4131a0367a76413
Patrick Downie <patdownie07@gmail.com>
2023-04-28 19:25:00 +0000

    made a silly game

---
+++ app.js
@@ -0,0 +1,29 @@
+"use strict";
+
+let noButton = document.getElementById("noButton");
+noButton.addEventListener("mouseover", moveButton);
+let yesButton = document.getElementById("yesButton");
+yesButton.addEventListener("click", thankYou);
+let thankYouText = document.getElementById("thankyoutext");
+
+let currentLocation = "avoid8";
+
+function generateRandomnumber(min, max) {
+  let number = Math.floor(min + Math.random() * (max + 1 - min));
+  return number;
+}
+
+function moveButton() {
+  console.log("mouseover");
+  while (noButton.className == currentLocation) {
+    noButton.className = "avoid" + generateRandomnumber(1, 14);
+  }
+  currentLocation = noButton.className;
+  console.log(noButton.className);
+}
+
+function thankYou() {
+  thankYouText.className = "revealed";
+  noButton.removeEventListener("mouseover", moveButton);
+  yesButton.removeEventListener("click", thankYou);
+}
